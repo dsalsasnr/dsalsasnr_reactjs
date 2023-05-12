5dd04595e4593f9ef96344f32f7d785e18cad29c
dsalsasnr <dsalsasnr@gmail.com>
2023-05-12 19:41:55 +0700

    Menambahkan tugas react js mengenai Lifecycle Component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import Form from "./Materi/Form/Pembahasan";
-
-// import "./Materi/Style/WebStyle.css";
-// import NavigationBar from "./Materi/StylingComponent/Navbar";
-// import Intro from "./Materi/StylingComponent/Intro";
-// import Content from "./Materi/StylingComponent/Content";
-
-// // import Komponen from "./Materi/Komponen";
-// import MyNavbar from "./Materi/Komponen/Pembahasan/Task-Basic-Component/Navbar";
-// //import MyComponent from "./Materi/Komponen/Pembahasan/Task-Basic-Component/Button";
-// import MyFooter from "./Materi/Komponen/Pembahasan/Task-Basic-Component/Footer";
-// import Content from "./Materi/Komponen/Pembahasan/Task-Basic-Component/Content";
-// import ClassComponent from "./Materi/Komponen/Pembahasan/ClassComponent";
+// import Form from "./Materi/Form/Pembahasan";
+import TaskNewsApp from "./Materi/LifecycleComponent/TaskNewsApp";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <TaskNewsApp />
     </div>
-
-    // <div className="App">
-    //   {/* Intro */}
-    //   <div className="webBg">
-    //     <NavigationBar />
-    //     <Intro />
-    //   </div>
-    //   <div className="content">
-    //     <Content />
-    //   </div>
-    //   <div className="end"></div>
-    //   {/* End of intro */}
-    // </div>
   );
 }
 
